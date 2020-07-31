31a938047ff5e44548377f1fa946674e5f238403
tekel90 <you@example.com>
2020-07-31 02:35:06 -0300

    Agrego ruta /

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const port = process.env.PORT || 8080;
 const about = require("./JSON/About.json");
 const portfolio = require("./JSON/Portfolio.json");
 
+app.get('/', (req, res) => {
+    res.send("Todo ok, funcionando")
+});
+
 app.get('/about', (req, res) => {
     res.json(about)
 });
